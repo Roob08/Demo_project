fd480198d56225957305488882d319d5af06ebc0
Afrakhan23 <afrakhan2308@gmail.com>
2022-12-01 16:55:28 +0530

    third commit

--- lib/form/form builder.dart
+++ lib/form/form builder.dart
@@ -17,7 +17,18 @@ class _formState extends State<form> {
   String? username;
   String? password;
   String? genderselected;
-  
+  String? slider;
+  String? Email;
+  String? Multiline;
+  String? PhoneNumber;
+  String? Radio;
+  String? Checkbox;
+  String? CheckBoxGroup;
+  String? segmented;
+  String? FilterChip;
+  String? choiceChip;
+  String? datetime;
+  String? DateTimePicker;
 
   @override
   Widget build(BuildContext context) {
@@ -46,7 +57,7 @@ class _formState extends State<form> {
                     });
                   },
                   validator: FormBuilderValidators.compose([
-                    FormBuilderValidators.required(       
+                    FormBuilderValidators.required(
                         errorText: "this field is required"),
                   ]),
                 ),
@@ -68,14 +79,17 @@ class _formState extends State<form> {
                         errorText: "this field is required"),
                   ]),
                 ),
-                FormBuilderDropdown(name: "gender", items: gender.map((e) => DropdownMenuItem(value: e,child: Text(e))).toList(),
-                    validator: FormBuilderValidators.compose([
+                FormBuilderDropdown(
+                  name: "gender",
+                  items: gender
+                      .map((e) => DropdownMenuItem(value: e, child: Text(e)))
+                      .toList(),
+                  validator: FormBuilderValidators.compose([
                     FormBuilderValidators.required(
-                        errorText: "this field is required"),]
-                
-                ),
-                // FormBuilderTextField(
-                //   name: "gender",
+                        errorText: "this field is required"),
+                  ]),
+                  // FormBuilderTextField(
+                  //   name: "gender",
                   decoration: InputDecoration(
                       hintText: "selectgender", labelText: "gender"),
                   // readOnly: false,
@@ -83,42 +97,210 @@ class _formState extends State<form> {
                   onChanged: (newValue) {
                     setState(() {
                       genderselected = newValue.toString();
-                     
                     });
                   },
                   // validator: FormBuilderValidators.compose([
                   //   FormBuilderValidators.required(
                   //       errorText: "this field is required"),
-                //   ]),
+                  //   ]),
                 ),
-                SizedBox(height: 20,),
-                ElevatedButton(onPressed: (() {
-                  if(_formKey.currentState!.validate()){
-                    _formKey.currentState!.save();
-                    username = _formKey.currentState!.value["username"].toString();
-                      password = _formKey.currentState!.value["password"].toString();
-                        genderselected = _formKey.currentState!.value["gender"].toString();
-                        Navigator.push(context,MaterialPageRoute(builder: (context) => demo(
-                           username:username!,
-                           password:password!,
-                           gender:genderselected!,
-                           
- 
-                        ))
-                        
-                        );
-                  }
-                  
-                }),
-                 child:Text("Login"), ),
+                FormBuilderSlider(
+                  name: "slider",
+                  initialValue: 40,
+                  min: 0,
+                  max: 100,
+                  divisions: 10,
+                  activeColor: Colors.red,
+                  inactiveColor: Colors.black,
+                ),
+                FormBuilderTextField(
+                  name: "Email",
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                      hintText: "Enter Your Email", labelText: "Email Address"),
+                  readOnly: false,
+                  enabled: true,
+                  onChanged: (value) {
+                    setState(() {
+                      Email = value;
+                      print(Email);
+                    });
+                  },
+                  validator: FormBuilderValidators.compose([
+                    FormBuilderValidators.required(
+                        errorText: "this field is required"),
+                    FormBuilderValidators.email()
+                  ]),
+                ),
+                FormBuilderTextField(
+                  name: "Multiline",
+                  keyboardType: TextInputType.multiline,
+                  maxLines: 10,
+                  minLines: 2,
+                  decoration: InputDecoration(
+                      hintText: "Enter Your Text", labelText: "Text"),
+                  readOnly: false,
+                  enabled: true,
+                  onChanged: (value) {
+                    setState(() {
+                      Multiline = value;
+                      print(Multiline);
+                    });
+                  },
+                ),
+                FormBuilderTextField(
+                  name: "Phone Number",
+                  keyboardType: TextInputType.number,
+                  maxLength: 10,
+                  decoration: InputDecoration(
+                      hintText: "Enter Your Number", labelText: "Number"),
+                  readOnly: false,
+                  enabled: true,
+                  onChanged: (value) {
+                    setState(() {
+                      PhoneNumber = value;
+                      print(PhoneNumber);
+                    });
+                  },
+                  validator: FormBuilderValidators.compose([
+                    FormBuilderValidators.required(
+                        errorText: "this field is required"),
+                    FormBuilderValidators.required()
+                  ]),
+                ),
+                FormBuilderRadioGroup(
+                  name: "Radio Button",
+                  // controlAffinity: ControlAffinity.trailing,
+                  options: ["c", "c++", "java"]
+                      .map((e) => FormBuilderFieldOption(
+                            value: e,
+                            child: Text(e),
+                          ))
+                      .toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      PhoneNumber = value;
+                      print(PhoneNumber);
+                    });
+                  },
+                  validator: FormBuilderValidators.compose([
+                    FormBuilderValidators.required(
+                        errorText: "this field is required"),
+                    FormBuilderValidators.numeric()
+                  ]),
+                ),
+                FormBuilderCheckbox(
+                  name: "Box",
+                  title: Row(
+                    children: [
+                      Text("Agree And Continue"),
+                      Text(
+                        " Terms And Continue",
+                        style: TextStyle(color: Colors.teal),
+                      )
+                    ],
+                  ),
+                  validator: FormBuilderValidators.compose([
+                    FormBuilderValidators.required(
+                        errorText: "this field is required"),
+                    FormBuilderValidators.required()
+                  ]),
+                ),
+                FormBuilderCheckboxGroup(
+                  name: "Check Box Group",
+                  options: List.generate(5, (index) => index + 1)
+                      .map((e) => FormBuilderFieldOption(
+                            value: e,
+                            child: Text(e.toString()),
+                          ))
+                      .toList(),
+                ),
+                // SizedBox(height: 10,),
+                FormBuilderSegmentedControl(
+                  name: "segmented",
+                  options: List.generate(5, (index) => index + 1)
+                      .map((e) => FormBuilderFieldOption(
+                          value: e, child: Icon(Icons.favorite)))
+                      .toList(),
+                  decoration: InputDecoration(
+                    labelText: "Segments",
+                  ),
+                ),
+                FormBuilderFilterChip(
+                  name: "Filter Chip",
+                  options: ["dart", "c", "c++"]
+                      .map((e) => FormBuilderChipOption(
+                          value: e, child: Text(e.toString())))
+                      .toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      FilterChip = value.toString();
+                      print(FilterChip);
+                    });
+                    decoration: InputDecoration(
+                     labelText: "select any option");
+                  },
+                ),
+                FormBuilderChoiceChip(name: "choice chip",
+                selectedColor: Colors.green[200],
+                 options:List.generate(5, (index) => index + 1)
+                      .map((e) => FormBuilderChipOption(
+                            value: e,
+                            child: Text(e.toString()),
+                          ))
+                      .toList(), ),
+                      FormBuilderDateTimePicker(name: "Date Time",fieldLabelText: "datetime",initialValue: DateTime.now(),firstDate: DateTime(2020,27,09),
+                      lastDate: DateTime.now(), 
+                       ),
+                       FormBuilderDateRangePicker(name: "DateTimePicker", firstDate: (" 2022,11,04"), lastDate: (2022,12,31), validator: FormBuilderValidators.compose([
+                    FormBuilderValidators.required(
+                        errorText: "this field is required"),
+
+                  ]),
+                  decoration: InputDecoration(hintText: "select time picker",labelText: "date time picker"),
+                  onChanged: ((value) {
+                    setState(() {
+                      DateTimePicker=value.toString();
+                      print(DateTimePicker);
+                    });
+                  }),
+                       
+                       
+                       
+                       
+                       
+                       ),
+                      
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: (() {
+                    if (_formKey.currentState!.validate()) {
+                      _formKey.currentState!.save();
+                      username =
+                          _formKey.currentState!.value["username"].toString();
+                      password =
+                          _formKey.currentState!.value["password"].toString();
+                      genderselected =
+                          _formKey.currentState!.value["gender"].toString();
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => demo(
+                                    username: username!,
+                                    password: password!,
+                                    gender: genderselected!,
+                                  )));
+                    }
+                  }),
+                  child: Text("Login"),
+                ),
               ],
             ),
           ),
-          
         ),
       ),
     );
   }
-  
- 
 }
